b7d38ccdb567d5e50cdbc19cf36747af6b1ed5a8
Ezewi Osorachukwu <oezewi@gmail.com>
2023-11-15 15:37:37 +0100

    mobile-responsive-fixed

--- src/components/Hambourger.jsx
+++ src/components/Hambourger.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import 'boxicons/css/boxicons.min.css';
+// import 'boxicons/css/boxicons.min.css';
 import { useState } from 'react';
 import Nav from './Nav';
 // import { Link } from 'react-router-dom';
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,4 +30,4 @@ export default {
   },
   plugins: [],
 }
-git commit -m "Responsive -mobile"
+
