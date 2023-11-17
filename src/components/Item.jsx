9cfae34e1b97508f3a8c8037b62e29120536bf6e
Ezewi Osorachukwu <oezewi@gmail.com>
2023-11-17 17:21:26 +0100

    agin today

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
 
-function Item({img, title, price,}) {
+function Item({ img, title, price }) {
   return (
-    <div className='productItem'>
-        <img className='productItem__img' src={img} alt="" />
+    <div className="productItem">
+      <img className="productItem__img" src={img} alt="" />
+      <div className="mt-5">
         <p>{title}</p>
         <p>{price}</p>
-        <Button text='click' bgColor='red'/>
-
+        <Button text="click" bgColor="red" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
